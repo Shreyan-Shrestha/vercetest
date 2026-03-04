576c9affa5faec67f1bf96f75405cadb050c2d7c
Shreyan Shrestha <shreyan.freelance@gmail.com>
2026-03-04 16:38:31 +0545

    changes to vercel.json

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 
-// Forward all requests to Laravel's real front controller
+// Go up one level from 'api/' folder to reach project root, then into 'public/'
 require __DIR__ . '/../public/index.php';
